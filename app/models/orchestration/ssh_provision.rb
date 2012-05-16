c88983e52983039736ff6159057c05856f9e4cbb
Ohad Levy <ohadlevy@gmail.com>
2012-05-16 14:54:31 +0300

    break down ssh tasks

--- app/models/orchestration/ssh_provision.rb
+++ app/models/orchestration/ssh_provision.rb
@@ -4,6 +4,7 @@ module Orchestration::SSHProvision
     base.class_eval do
       attr_reader :image_id
       after_validation :validate_ssh_provisioning, :queue_ssh_provision
+      attr_accessor :image, :template_file, :client
     end
   end
 
@@ -20,60 +21,50 @@ module Orchestration::SSHProvision
 
     # I guess this is not going to happen on create as we might not have an ip address yet.
     def queue_ssh_provision_create
-      queue.create(:name   => "Configuring instance #{self} via SSH", :priority => 2000,
+
+      queue.create(:name   => "Preparing Post installation script for #{self}", :priority => 2000,
+                   :action => [self, :setSSHProvisionScript])
+      queue.create(:name   => "Waiting for #{self} to come online", :priority => 2001,
+                   :action => [self, :setSSHWaitForResponse])
+      queue.create(:name   => "Enable Certificate generation for #{self}", :priority => 2002,
+                   :action => [self, :setSSHEnableCert])
+      queue.create(:name   => "Configuring instance #{self} via SSH", :priority => 2003,
                    :action => [self, :setSSHProvision])
     end
 
     def queue_compute_update;
     end
 
-    def setSSHProvision
+    def setSSHProvisionScript
       logger.info "About to start post launch script on #{name}"
-      image    = Image.find_by_uuid(image_id)
-      template = configTemplate(:kind => "finish")
-      @host    = self
+      self.image = Image.find_by_uuid(image_id)
+      template   = configTemplate(:kind => "finish")
+      @host      = self
       logger.info "generating template to upload to #{name}"
-      file = unattended_render_to_temp_file(template.template)
-
-      start_ssh_provisioning image.username, file
-
-    rescue => e
-      failure "Failed to start SSH provisioning task for #{name}: #{e}", e.backtrace
+      self.template_file = unattended_render_to_temp_file(template.template)
     end
 
-    def delSSHProvision
-      # since we enable certificates/autosign via here, we also need to make sure we clean it up in case of an error
-      if puppetca?
-        respond_to?(:initialize_puppetca) && initialize_puppetca && delCertificate && delAutosign
-      end
-    rescue => e
-      failure "Failed to remove certificates for #{name}: #{e}", e.backtrace
+    def delSSHProvisionScript;
     end
 
-    def validate_ssh_provisioning
-      return unless ssh_provision?
-      return if Rails.env == "test"
-      status = true
-      unless configTemplate(:kind => "finish")
-        status = failure "No finish templates were found for this host, make sure you define at least one in your #{os} settings"
-      end
-      unless Image.find_by_uuid(image_id)
-        status &= failure("failed to find instance image #{image_id}")
-      end
-
-      status
-
+    def setSSHWaitForResponse
+      logger.info "Starting SSH provisioning script - waiting for #{ip} to respond"
+      self.client = Foreman::Provision::SSH.new ip, image.username, :template => template_file.path, :uuid => uuid, :key_data => [compute_resource.key_pair.secret]
     end
 
-    private
+    def delSSHWaitForResponse;
+    end
 
-    def start_ssh_provisioning username, file
-
+    def setSSHEnableCert
       self.handle_ca
       return false if errors.any?
       logger.info "Revoked old certificates and enabled autosign"
-      logger.info "Starting SSH provisioning script - waiting for #{ip} to respond"
-      client = Foreman::Provision::SSH.new ip, username, :template => file.path, :uuid => uuid, :key_data => [compute_resource.key_pair.secret]
+    end
+
+    def delSSHEnableCert;
+    end
+
+    def setSSHProvision
       logger.info "SSH connection established to #{ip} - executing template"
       if client.deploy!
         self.build        = false
@@ -87,4 +78,29 @@ module Orchestration::SSHProvision
     end
 
   end
+
+  def delSSHProvision
+    # since we enable certificates/autosign via here, we also need to make sure we clean it up in case of an error
+    if puppetca?
+      respond_to?(:initialize_puppetca) && initialize_puppetca && delCertificate && delAutosign
+    end
+  rescue => e
+    failure "Failed to remove certificates for #{name}: #{e}", e.backtrace
+  end
+
+  def validate_ssh_provisioning
+    return unless ssh_provision?
+    return if Rails.env == "test"
+    status = true
+    unless configTemplate(:kind => "finish")
+      status = failure "No finish templates were found for this host, make sure you define at least one in your #{os} settings"
+    end
+    unless Image.find_by_uuid(image_id)
+      status &= failure("failed to find instance image #{image_id}")
+    end
+
+    status
+
+  end
+
 end
